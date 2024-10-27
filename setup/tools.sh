8fbd755d73562782a3fed19d63faa2b5c207c3c4
Tan Nguyen <tannp@cslant.com>
2024-10-27 22:28:03 +0700

    fix: update tool for prod

--- setup/tools.sh
+++ setup/tools.sh
@@ -1,4 +1,6 @@
-if ! command -v pm2 &> /dev/null; then
-  echo '  ∟ Installing pm2...'
-  npm install -g pm2
-fi
+if [ "$ENV" = "prod" ]; then
+  if ! command -v pm2 &> /dev/null; then
+    echo '  ∟ Installing pm2...'
+    npm install -g pm2
+  fi
+else
